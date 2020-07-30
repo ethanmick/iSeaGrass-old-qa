c09942292a29da6e6a176232b9110f814b348ebb
Ethan Mick <ethan@ethanmick.com>
2020-07-30 18:56:41 -0400

    Update hooks to query any datastore easily

--- hooks/useDB.ts
+++ hooks/useDB.ts
@@ -1,8 +1,16 @@
 import { DatabaseContext } from 'contexts/DatabaseContext'
-import { Database } from 'db'
+import {
+  Database,
+  DROP_FRAME_STORE,
+  SAMPLE_STORE,
+  STATION_STORE,
+  STORES,
+  TRIP_STORE
+} from 'db'
 import { IDBPDatabase } from 'idb'
-import { Trip } from 'models'
+import { DropFrame, Sample, Station, Trip } from 'models'
 import { useContext, useEffect, useState } from 'react'
+import { QueryParam, useQuery } from './useQuery'
 
 export const useDB = () => useContext(DatabaseContext)
 
@@ -22,24 +30,27 @@ export const useDBQuery = <T>(fn: UseTripsAccessor): QueryHook<T> => {
   return { result, error }
 }
 
-interface TripHook {
+interface StoreByKeyHook<T> {
   db: IDBPDatabase<Database>
-  trip?: Trip
+  value?: T
   error?: Error
   loading: boolean
 }
 
-export const useTrip = (id: number | string | undefined): TripHook => {
+const useStoreByKey = <T>(
+  store: STORES,
+  param: QueryParam
+): StoreByKeyHook<T> => {
+  const id = useQuery(param)
   const { db } = useDB()
   const [loading, setLoading] = useState<boolean>(true)
-  const [trip, setTrip] = useState<Trip | undefined>(undefined)
+  const [value, setValue] = useState<T | undefined>(undefined)
   const [error, setError] = useState<Error | undefined>(undefined)
   useEffect(() => {
     ;(async function () {
       if (db && id) {
-        const query = typeof id == 'string' ? parseInt(id) : id
         try {
-          setTrip(await db.get('trips', query))
+          setValue((await db.get(store, id)) as any)
         } catch (err) {
           setError(err)
         } finally {
@@ -48,5 +59,23 @@ export const useTrip = (id: number | string | undefined): TripHook => {
       }
     })()
   }, [db, id])
-  return { db, loading, trip, error }
+  return { db, loading, value, error }
 }
+
+// useTrip loads the trip from the query param `tripId`.
+// This should be used throughout the app in any component you want to load
+// the trip. Most often this is the top level pages.
+export const useTrip = (): StoreByKeyHook<Trip> =>
+  useStoreByKey(TRIP_STORE, 'tripId')
+
+// useStation
+export const useStation = (): StoreByKeyHook<Station> =>
+  useStoreByKey(STATION_STORE, 'stationId')
+
+// useDropFrame
+export const useDropFrame = (): StoreByKeyHook<DropFrame> =>
+  useStoreByKey(DROP_FRAME_STORE, 'dropFrameId')
+
+// useSample
+export const useSample = (): StoreByKeyHook<Sample> =>
+  useStoreByKey(SAMPLE_STORE, 'sampleId')
--- hooks/useQuery.ts
+++ hooks/useQuery.ts
@@ -1,6 +1,12 @@
 import { useRouter } from 'next/router'
 
-export const useQuery = (q: string): number | undefined => {
+export const TRIP_QUERY = 'tripId'
+export const STATION_QUERY = 'stationId'
+export const DROP_FRAME_QUERY = 'dropFrameId'
+export const SAMPLE_QUERY = 'sampleId'
+export type QueryParam = 'tripId' | 'stationId' | 'dropFrameId' | 'sampleId'
+
+export const useQuery = (q: QueryParam): number | undefined => {
   const router = useRouter()
   return router.query[q] ? parseInt(router.query[q] as string) : undefined
 }
