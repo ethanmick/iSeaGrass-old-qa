fde0868f02dc6f5a77857d96ec76070f25ceb9b3
Ethan Mick <ethan@ethanmick.com>
2021-07-12 21:44:10 -0400

    Fix IDB on Safari, update protocol pdf

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,13 +7,14 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
 import 'react-datepicker/dist/react-datepicker.css'
+import idbReady from 'safari-14-idb-fix'
 import 'styles/index.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [db, setDB] = useState<IDBPDatabase<Database>>(undefined)
   // Open connection on app initialization
   useEffect(() => {
-    connect().then(setDB)
+    idbReady().then(connect).then(setDB)
   }, [])
   return (
     <DatabaseContext.Provider value={{ db, setDB }}>
